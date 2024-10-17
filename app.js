5dd52bbd20dbc35930513d99ca733c0bff83e4f7
sreyesg <santiagoreyes.g@gmail.com>
2024-10-16 20:06:05 -0700

    Resolve exercise 3 to 6

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ------ Exercise 1
 // Use `.map()` to iterate over the following array:
 
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
@@ -10,6 +11,7 @@ const multiByTwo = nums.map((num) => {
     return num * 2
 })
 
+// ---------Exercise 2
 // console.log(multiByTwo)
 
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
@@ -19,8 +21,67 @@ const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // Your code here
 const [toppingOne, toppingTwo] = pizzaToppings
-console.log(toppingOne)
-console.log(toppingTwo)
+// console.log(toppingOne)
+// console.log(toppingTwo)
 
+
+// ----------Exercise 3
 // Elements are match on their name
 
+// Given the following object, use destructuring to 
+// create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Your code here
+  
+const {make, model} = car;
+console.log(make)
+console.log(model)
+
+
+// -------Exercise 4
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings2 = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const duplicateToppings = [...pizzaToppings2]
+
+// ----- Exercise 5
+
+// Duplicate the following object and spread its values into a new variable
+//  `myCar`.
+
+const car2 = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Change the `model` property of `myCar` to 'q7'. Log both objects.
+  
+  // Your code here
+
+const myCar = {...car2}
+myCar.model = 'q7'
+console.log('original', car2)
+console.log('modified', myCar)
+
+//  Exercise 6: Dynamic keys in objects
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it
+    //  (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, 
+    // assigning a relevant value.
+
+// Your code here
+
+const propertyName = 'username'
+const userProfile = {
+    [propertyName]: 'Santie25'
+}
